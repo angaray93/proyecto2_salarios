4e311163810e6aefa866367a1c2c5d817924cb9f
anragale <angaray93@gmail.com>
2018-02-28 17:15:21 -0300

    Implementacion de busqueda de funcionarios previo a la creacion de movimiento.

--- liquidacion/urls.py
+++ liquidacion/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     #path('categoriasalarial_list', views.categoriasalarial_list, name='categoriasalarial_list'),
     #path('categoriasalarial/create/', views.vista_categoriasalarial, name='nueva_categoria'),
     path('opciones_proceso', views.opciones_proceso, name='opciones_proceso'),
-    path('movimiento/nuevo/', views.movimiento_vista, name='nuevo_movimiento'),
+    path('funcionario/<int:idfuncionario>/movimiento/nuevo/', views.movimiento_vista, name='nuevo_movimiento'),
     path('movimiento/<int:idmovimiento>', views.movimiento_vista, name='modificar_movimiento'),
+    path('movimiento/<int:idmovimiento>', views.movimiento_vista, name='ver_movimiento'),
     path('buscar_funcionario', views.busqueda_funcionarios, name='buscar_funcionario'),
 
 ]
--- liquidacion/views.py
+++ liquidacion/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse
 
-from liquidacion.filters import FuncionarioFilter
 from liquidacion.forms import *
 from liquidacion.models import *
 
@@ -14,7 +13,7 @@ def index(request):
     return render(request, 'index.html')
 
 
-def movimiento_vista(request, idmovimiento=None, idpadre=None):
+def movimiento_vista(request, idmovimiento=None, idfuncionario=None):
     context = {}
     movimiento = None
     if request.POST:
@@ -25,7 +24,7 @@ def movimiento_vista(request, idmovimiento=None, idpadre=None):
         form = MovimientoForm(request.POST, instance=movimiento)
         if form.is_valid():
             movimiento = form.save()
-            #return redirect(reverse('Core:editar_cuenta', args=[movimiento.pk]))
+            return redirect(reverse('liquidacion:ver_movimiento', args=[movimiento.pk]))
         else:
             # TODO Implementar sistema de errores
             context.update({
@@ -42,10 +41,12 @@ def movimiento_vista(request, idmovimiento=None, idpadre=None):
                 'form': form
             })
         else:
+            funcionario = Funcionario.objects.get(pk=idfuncionario)
             form = MovimientoForm(initial={
-                #'cuenta': default_cuenta,
+                'funcionario': funcionario,
             })
             context.update({
+                'funcionario': funcionario,
                 'form': form,
             })
         return render(request, 'proceso/movimiento_form.html', context)
@@ -59,8 +60,7 @@ def busqueda_funcionarios(request):
     queryset_list = Funcionario.objects.all()
     recibido = request.GET.get("q")
     if recibido:
-        queryset_list = queryset_list.filter(nombres__icontains=recibido)
+        queryset_list = queryset_list.filter\
+            (Q(nombres__icontains=recibido) | Q(apellidos__icontains=recibido) | Q(cedula__icontains=recibido))
 
-    #funcionario_list = Funcionario.objects.all()
-    #funcionario_filter = FuncionarioFilter(request.GET, queryset=funcionario_list)
     return render(request, 'proceso/filtro_funcionarios_movimiento.html', {'funcionarios': queryset_list})
